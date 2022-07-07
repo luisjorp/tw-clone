c1a7b4268c31dd302fc36ad6ffa101b8b7e73740
LuisJo Ruano <luis.jose@arcadalabs.com>
2022-07-07 09:25:25 -0600

    ADD: prettier

--- components/Feed.tsx
+++ components/Feed.tsx
@@ -1,20 +1,23 @@
-import * as React from 'react';
-import {RefreshIcon} from '@heroicons/react/outline';
-import TweetBox from './TweetBox';
+import * as React from "react";
+import { RefreshIcon } from "@heroicons/react/outline";
+import TweetBox from "./TweetBox";
 
 export default function Feed() {
-    return (<div className="col-span-7 lg:col-span-5 border-x">
-        <div className="flex items-center justify-between">
-            <h1 className="p-5 pb-0 text-xl font-bold">Home</h1>
-            <RefreshIcon className="h-8 w-8 cursor-pointer
-                text-twitter mr-5 mt-5 transition-all duration-500 ease-out
-                hover:rotate-180 active:scale-75"/>
-        </div>
+  return (
+    <div className="col-span-7 grid border-x lg:col-span-5">
+      <div className="flex items-center justify-between">
+        <h1 className="p-5 pb-0 text-xl font-bold">Home</h1>
+        <RefreshIcon
+          className="mr-5 mt-5 h-8
+                w-8 cursor-pointer text-twitter transition-all duration-500 ease-out
+                hover:rotate-180 active:scale-75"
+        />
+      </div>
 
-        {/*tweetbox*/}
-        <div>
-            <TweetBox/>
-        </div>
-
-    </div>);
+      {/*tweetbox*/}
+      <div>
+        <TweetBox />
+      </div>
+    </div>
+  );
 }
--- components/Widgets.tsx
+++ components/Widgets.tsx
@@ -1,20 +1,24 @@
-import * as React from 'react';
-import {SearchIcon} from '@heroicons/react/outline';
-import {TwitterTimelineEmbed} from 'react-twitter-embed';
+import * as React from "react";
+import { SearchIcon } from "@heroicons/react/outline";
+import { TwitterTimelineEmbed } from "react-twitter-embed";
 
 export default function Widgets() {
-    return (<div className="mt-2 px-2 col-span-2 hidden lg:inline">
-        {/*Search*/}
-        <div className="flex items-center space-x-2 bg-gray-100 rounded-full p-3">
-            <SearchIcon className="w-5 h-5 text-gray-400"/>
-            <input className="bg-transparent flex-1 outline-none"
-                   type="text"
-                   placeholder="Search Twitter"/>
-        </div>
-        <TwitterTimelineEmbed
-            sourceType="profile"
-            screenName="sonnysangha"
-            options={{height: 1000}}
+  return (
+    <div className="col-span-2 mt-2 hidden px-2 lg:inline">
+      {/*Search*/}
+      <div className="flex items-center space-x-2 rounded-full bg-gray-100 p-3">
+        <SearchIcon className="h-5 w-5 text-gray-400" />
+        <input
+          className="flex-1 bg-transparent outline-none"
+          type="text"
+          placeholder="Search Twitter"
         />
-    </div>);
-};
+      </div>
+      <TwitterTimelineEmbed
+        sourceType="profile"
+        screenName="sonnysangha"
+        options={{ height: 1000 }}
+      />
+    </div>
+  );
+}
