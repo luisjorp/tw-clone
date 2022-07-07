f8cc2a9e6ad8508f93ff8b4b83a735b4fa301f40
LuisJo Ruano <luis.jose@arcadalabs.com>
2022-07-07 09:15:15 -0600

    ADD: tweetbox

--- components/TweetBox.tsx
+++ components/TweetBox.tsx
@@ -1,4 +1,5 @@
 import * as React from 'react';
+import {useState} from 'react';
 import Image from 'next/image';
 import {
     CalendarIcon,
@@ -9,6 +10,8 @@ import {
 } from '@heroicons/react/outline';
 
 export default function TweetBox() {
+    const [input, setInput] = useState<string>('');
+
     return (<div className="flex space-x-2 p-5">
         <div className="h-14 w-14 relative mt-4">
             <Image width="100%"
@@ -19,16 +22,23 @@ export default function TweetBox() {
         </div>
         <div className="flex flex-1 items-center pl-2">
             <form className="flex flex-1 flex-col">
-                <input className="h-24 w-full text-xl outline-none placeholder:text-xl" type="text" placeholder="What's happening?"/>
+                <input className="h-24 w-full text-xl outline-none placeholder:text-xl" type="text"
+                       value={input}
+                       onChange={e => setInput(e.target.value)}
+                       placeholder="What's happening?"/>
                 <div className="flex items-center">
                     <div className="flex flex-1 space-x-2 text-twitter">
-                        <PhotographIcon className="h-5 w-5 cursor-pointer transition-transform duration-150 ease-out hover:scale-150"/>
+                        <PhotographIcon
+                            className="h-5 w-5 cursor-pointer transition-transform duration-150 ease-out hover:scale-150"/>
                         <SearchCircleIcon className="h-5 w-5"/>
                         <EmojiHappyIcon className="h-5 w-5"/>
                         <CalendarIcon className="h-5 w-5"/>
                         <LocationMarkerIcon className="h-5 w-5"/>
                     </div>
-                    <button className="bg-twitter px-5 py-2 font-bold rounded-full text-white">Tweet</button>
+                    <button
+                        className="bg-twitter px-5 py-2 font-bold rounded-full text-white disabled:opacity-40"
+                        disabled={!input}>Tweet
+                    </button>
                 </div>
             </form>
         </div>
